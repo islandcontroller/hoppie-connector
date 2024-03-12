70fe4b07cfad0be7859fa779c119951d8babffc6
islandcontroller <13700483+islandcontroller@users.noreply.github.com>
2024-03-12 23:18:55 +0000

    Added docstrings for public-facing classes and methods

--- src/hoppie_connector/Messages.py
+++ src/hoppie_connector/Messages.py
@@ -3,6 +3,10 @@ import re
 from datetime import datetime, time, UTC
 
 class HoppieMessage(object):
+    """HoppieMessage(from_name, to_name, type)
+    
+    Abstract base message object
+    """
     class MessageType(enum.Enum):
         ADS_C = 'ads-c'
         PROGRESS = 'progress'
@@ -14,6 +18,17 @@ class HoppieMessage(object):
         return name == 'ALL-CALLSIGNS' or re.match(r'^[A-Z0-9]{3,8}$', name)
 
     def __init__(self, from_name: str, to_name: str, type: MessageType):
+        """Create base message object
+
+        Note:
+            `from_name` and `to_name` must be valid station names (ICAO flight
+            number, 3-letter org code or special station names)
+
+        Args:
+            from_name (str): Sender station name
+            to_name (str): Recipient station name
+            type (MessageType): Message type code
+        """
         if type not in self.MessageType:
             raise ValueError('Invalid message type')
         elif not self._is_valid_station_name(from_name):
@@ -26,18 +41,28 @@ class HoppieMessage(object):
             self._type = type
 
     def get_from_name(self) -> str:
+        """Return sender station name
+        """
         return self._from
 
     def get_to_name(self) -> str:
+        """Return recipient station name
+        """
         return self._to
 
     def get_msg_type(self) -> MessageType:
+        """Return message type code
+        """
         return self._type
 
     def get_packet_content(self) -> str:
+        """Return encoded packet content
+        """
         return ''
 
     def get_msg_params(self) -> dict:
+        """Return collated metadata
+        """
         return {
             'from': self._from,
             'to': self._to,
@@ -60,9 +85,20 @@ class PollMessage(HoppieMessage):
         super().__init__(from_name, 'SERVER', self.MessageType.POLL)
 
 class TelexMessage(HoppieMessage):
+    """TelexMessage(from_name, to_name, message)
+
+    Freetext ACARS message
+    """
     _TELEX_MAX_MSG_LEN: int = 220
 
     def __init__(self, from_name: str, to_name: str, message: str):
+        """Create a freetext message
+
+        Args:
+            from_name (str): Sender station name
+            to_name (str): Recipient station name
+            message (str): Message content
+        """
         if len(message) > self._TELEX_MAX_MSG_LEN: 
             raise ValueError('Message too long')
         elif not message.isascii():
@@ -72,17 +108,36 @@ class TelexMessage(HoppieMessage):
             self._message = message
 
     def get_message(self) -> str:
+        """Return freetext message content
+        """
         return self._message
 
     def get_packet_content(self) -> str:
         return self._message.upper()
 
 class ProgressMessage(HoppieMessage):
+    """ProgressMessage(from_name, to_name, dep, arr, time_out[, time_eta[, time_off[, time_on[, time_in]]]])
+    
+    ACARS OOOI (Out-off-on-in) Report
+    """
     @classmethod
     def _Is_valid_aprt_icao(cls, input: str) -> bool:
         return bool(re.match(r'^[A-Z]{4}$', input))
 
     def __init__(self, from_name: str, to_name: str, dep: str, arr: str, time_out: time, time_eta: time | None = None, time_off: time | None = None, time_on: time | None = None, time_in: time | None = None):
+        """Create a progress message
+
+        Args:
+            from_name (str): Sender station name
+            to_name (str): Recipient station name
+            dep (str): Departure airport ICAO code
+            arr (str): Destination/Arrival airport ICAO code
+            time_out (time): OUT time
+            time_eta (time | None, optional): Estimated time of arrival. Defaults to None.
+            time_off (time | None, optional): OFF time. Defaults to None.
+            time_on (time | None, optional): ON time. Defaults to None.
+            time_in (time | None, optional): IN time. Defaults to None.
+        """
         if not self._Is_valid_aprt_icao(dep):
             raise ValueError('Invalid departure identifier')
         elif not self._Is_valid_aprt_icao(arr):
@@ -106,24 +161,38 @@ class ProgressMessage(HoppieMessage):
             self._eta = time_eta
 
     def get_departure(self) -> str:
+        """Return departure airport code
+        """
         return self._dep
 
     def get_arrival(self) -> str:
+        """Return arrival airport code
+        """
         return self._arr
 
     def get_time_out(self) -> time:
+        """Return OUT time
+        """
         return self._out
 
     def get_time_off(self) -> time | None:
+        """Return OFF time if specified
+        """
         return self._off
 
     def get_time_on(self) -> time | None:
+        """Return ON time if specified
+        """
         return self._on
 
     def get_time_in(self) -> time | None:
+        """Return IN time if specified
+        """
         return self._in
 
     def get_eta(self) -> time | None:
+        """Return ETA if specified
+        """
         return self._eta
 
     def get_packet_content(self) -> str:
@@ -143,6 +212,10 @@ class ProgressMessage(HoppieMessage):
         return packet
 
 class AdscMessage(HoppieMessage):
+    """AdscMessage(from_name, to_name, report_time, posotion, altitude[, heading[, remark]])
+    
+    ADC-C Position Report
+    """
     def __init__(self, from_name: str, to_name: str, report_time: datetime, position: tuple[float, float], altitude: float, heading: float | None = None, remark: str | None = None):
         if position[0] < -90.0 or position[0] > 90.0:
             raise ValueError('Latitude out of range')
@@ -163,18 +236,31 @@ class AdscMessage(HoppieMessage):
             self._remark = remark
 
     def get_report_time(self) -> datetime:
+        """Return time and day(!) of report
+
+        Note:
+            Month and year information must be ignored for received messages
+        """
         return self._time
 
     def get_position(self) -> tuple[float, float]:
+        """Return position information (lat, lon)
+        """
         return self._position
 
     def get_altitude(self) -> float:
+        """Return altitude information (feet)
+        """
         return self._altitude
 
     def get_heading(self) -> float | None:
+        """Return heading information
+        """
         return self._heading
 
     def get_remark(self) -> str | None:
+        """Return remark section contents
+        """
         return self._remark
 
     def get_packet_content(self) -> str:
--- src/hoppie_connector/__init__.py
+++ src/hoppie_connector/__init__.py
@@ -7,7 +7,20 @@ class HoppieError(Exception): pass
 class HoppieWarning(UserWarning): pass
 
 class HoppieConnector(object):
+    """HoppieConnector(station_name, logon)
+
+    Connector for interacting with Hoppie's ACARS service.
+    """
     def __init__(self, station_name: str, logon: str):
+        """Create a new connector
+
+        Note:
+            Station name must be a valid ICAO flight number or 3-letter org code.
+
+        Args:
+            station_name (str): Own station name
+            logon (str): API logon code
+        """
         self._f = HoppieMessageFactory(station_name)
         self._api = HoppieAPI(logon)
 
@@ -32,10 +45,39 @@ class HoppieConnector(object):
             raise NotImplementedError()
 
     def peek(self) -> list[tuple[int, HoppieMessage]]:
+        """Peek all messages destined to own station
+
+        Note:
+            Own station will not appear as 'online'. Peeked messages will not
+            be marked as relayed. Message history is kept on the server for up 
+            to 24 hours.
+
+        Returns:
+            list[tuple[int, HoppieMessage]]: List of messages (id, content)
+        """
         return self._connect(self._f.create_peek())
 
     def poll(self) -> list[tuple[int, HoppieMessage]]:
+        """Poll for new messages destined to own station and mark them as relayed.
+
+        Note:
+            Polling will make the own station name appear as 'online' and mark
+            received messages as 'relayed'. Previously relayed messages will 
+            not reappear in the next `poll` response.
+
+        Returns:
+            list[tuple[int, HoppieMessage]]: List of messages (id, content)
+        """
         return self._connect(self._f.create_poll())
     
     def send_telex(self, to_name: str, message: str) -> None:
+        """Send a freetext message to recipient station.
+
+        Note:
+            Message length is limited to 220 characters by ACARS specification.
+
+        Args:
+            to_name (str): Recipient station name
+            message (str): Message content
+        """
         self._connect(self._f.create_telex(to_name, message))
