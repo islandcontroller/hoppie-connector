06a06dba969efe694ec8bf0cec44ef0e9304d476
islandcontroller <13700483+islandcontroller@users.noreply.github.com>
2024-04-12 23:53:36 +0000

    Added AdscMessageParser tests

---
+++ tests/test_AdscMessageParser.py
@@ -0,0 +1,16 @@
+from hoppie_connector.Messages import AdscMessageParser, AdscPeriodicContractRequestMessage, AdscPeriodicReportMessage
+import unittest
+
+class TestAdscMessageParser(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self._UUT = AdscMessageParser()
+
+    def test_periodic_contract_request(self):
+        self.assertIsInstance(self._UUT.from_packet('ATC', 'CALLSIGN', 'REQUEST PERIODIC 120'), AdscPeriodicContractRequestMessage)
+
+    def test_periodic_report(self):
+        self.assertIsInstance(self._UUT.from_packet('CALLSIGN', 'ATC', 'REPORT CALLSIGN 011820 -10.0000 10.00000 3000'), AdscPeriodicReportMessage)
+
+    def test_unknown_format(self):
+        self.assertRaises(ValueError, lambda: self._UUT.from_packet('ATC', 'CALLSIGN', 'REQUEST EVENT'))
