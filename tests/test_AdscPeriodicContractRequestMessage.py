c0b9eed3e396782757ca721574414c365f9bd818
islandcontroller <13700483+islandcontroller@users.noreply.github.com>
2024-04-12 23:48:37 +0000

    Added AdscPeriodicContractRequestMessage tests

---
+++ tests/test_AdscPeriodicContractRequestMessage.py
@@ -0,0 +1,42 @@
+from hoppie_connector.Messages import AdscPeriodicContractRequestMessage
+import unittest
+
+class TestAdscPeriodicContractRequestMessage(unittest.TestCase):
+    _EXPECTED_INTERVAL: int = 120
+    
+    def setUp(self) -> None:
+        super().setUp()
+        self._UUT = AdscPeriodicContractRequestMessage('ATC', 'CALLSIGN', self._EXPECTED_INTERVAL)
+
+    def test_get_interval(self):
+        self.assertEqual(self._EXPECTED_INTERVAL, self._UUT.get_interval())
+
+    def test_not_demand_contract_request(self):
+        self.assertFalse(self._UUT.is_demand_contract_request())
+
+class TestAdscPeriodicContractRequestMessageDemandRequest(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self._UUT = AdscPeriodicContractRequestMessage('ATC', 'CALLSIGN', 0)
+
+    def test_is_demand_contract_request(self):
+        self.assertTrue(self._UUT.is_demand_contract_request())
+
+class TestAdscPeriodicContractRequestMessageErrorHandling(unittest.TestCase):
+    def test_invalid_interval(self):
+        self.assertRaises(ValueError, lambda: AdscPeriodicContractRequestMessage('ATC', 'CALLSIGN', -1))
+
+class TestAdscPeriodicContractRequestMessageRepresentation(unittest.TestCase):
+    def test_repr(self):
+        expected = AdscPeriodicContractRequestMessage('ATC', 'CALLSIGN', 120)
+        actual = eval(repr(expected))
+        self.assertEqual(expected, actual)
+
+class TestAdscPeriodicContractRequestMessageFromPacket(unittest.TestCase):
+    def test_from_packet(self):
+        expected = AdscPeriodicContractRequestMessage('ATC', 'CALLSIGN', 120)
+        actual = AdscPeriodicContractRequestMessage.from_packet('ATC', 'CALLSIGN', "REQUEST PERIODIC 120")
+        self.assertEqual(expected, actual)
+
+    def test_invalid_format(self):
+        self.assertRaises(ValueError, lambda: AdscPeriodicContractRequestMessage.from_packet('ATC', 'CALLSIGN', 'REQUEST PERIODIC'))
